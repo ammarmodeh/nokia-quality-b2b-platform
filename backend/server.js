d6d15929e3c22af34558b0a851f5fc36df34937c
ammarmodeh <ammarm.odeh@gmail.com>
2025-04-08 02:40:24 +0300

    Update fe and be

--- backend/server.js
+++ backend/server.js
@@ -50,5 +50,7 @@ app.use("/api/customer-issues-notifications", customerIssueRoutes);
 app.use("/api/suggestions", suggestionsRoutes);
 app.use("/api/policies", policyRoutes);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
