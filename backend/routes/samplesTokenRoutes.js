a820768bebd04616551c04aec9f762b728f4d1dd
ammarmodeh <ammarm.odeh@gmail.com>
2025-12-17 11:48:35 +0300

    samplesTokenRoutes.js Update

--- backend/routes/samplesTokenRoutes.js
+++ backend/routes/samplesTokenRoutes.js
@@ -9,11 +9,20 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+// Debug middleware for this router
+router.use((req, res, next) => {
+  console.log(`[samplesTokenRoutes] Entered: ${req.method} ${req.url}`);
+  next();
+});
+
 // All routes require authentication
 router.use(protect);
 
 // GET /api/samples-token/:year - Get all samples for a year
-router.get('/:year', getSamplesByYear);
+router.get('/:year', (req, res, next) => {
+  console.log(`[samplesTokenRoutes] GET /:year hit. Param: ${req.params.year}`);
+  getSamplesByYear(req, res, next);
+});
 
 // POST /api/samples-token/bulk - Bulk save/update samples
 router.post('/bulk', bulkSaveSamples);
